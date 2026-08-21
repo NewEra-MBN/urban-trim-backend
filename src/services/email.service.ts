4c38b5c7458d394323902839dc6d68a528fc2111
Monsur Bin Numan <monsurbinnuman24@gmail.com>
2026-08-22 07:10:00 +0900

    feat: implemented email service

---
+++ src/services/email.service.ts
@@ -0,0 +1,40 @@
+import nodemailer from 'nodemailer'
+import config from '../config/config.js'
+//import logger 
+
+const transport = nodemailer.createTransport(config.email.smtp);
+
+if(config.env !== 'test') {
+    transport
+        .verify()
+        .then(() => console.log('connected to email server'))
+        .catch(() => console.log('failed to connect email serverr'))
+}
+
+
+const sendMail = async(to: string, subject: string, text: string) => {
+    const msg = { from: config.email.from, to, subject, text }
+    await transport.sendMail(msg)
+}   
+
+
+
+const sendResetPasswordEmail = async(to : string, token : string) => {
+    const subject = 'Reset Password';
+    const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
+    const text = `Dear User
+        To reset your password please click on the link ${resetPasswordUrl}.
+        If you did not request any password reset please ignore the email.
+    `;
+    await sendMail(to, subject, text)
+}
+
+
+const sendVerificationEmail = async(to: string, token : string) => {
+    const subject = 'verify Email';
+    const verifyEmailUrl = `http://link-to-app/verify-email?token=${token}`;
+    const text = `Dear User
+        for verifying the email please click on the link ${verifyEmailUrl}
+    `
+    await sendMail(to, subject, text);
+}
