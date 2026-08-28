37c000ff433f0a435c312b38fd2826eced2a369d
Monsur Bin Numan <monsurbinnuman24@gmail.com>
2026-08-28 20:23:24 +0900

    feat: pick function written

---
+++ src/utils/pick.ts
@@ -0,0 +1,8 @@
+const pick =<T extends object, Key extends keyof T> (obj: T, keys: Key[]): Pick<T, Key> => {
+    return keys.reduce((finalObj, key) => {
+        if(Object.hasOwn(obj, key)){
+            finalObj[key] = obj[key]
+        }
+        return finalObj
+    },{} as Pick<T, Key>)
+}
